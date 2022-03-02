ef3c4086873230d510af8345bd7e886683ba7b4b
oscarf4 <oscarvedo97@gmail.com>
2022-03-01 22:45:02 -0500

    Seal() y freeze(): Seal() pone configurable en FALSE y Freeze() configurable y writable en FALSE. NOTA: AMBOS HACEN QUE EL OBJETO NO PUEDA RECIBIR NI ELIMINAR PROPIEDADES

--- POO/Static/metodos-estaticos.js
+++ POO/Static/metodos-estaticos.js
@@ -14,32 +14,39 @@ const oscar = {
 
 
 //Crear propiedades en nuestro objeto, pudiendo editar los DESCRIPTORS
-Object.defineProperty(oscar, 'pruebaNasa', {
-    value: 'Extraterrestres',
-    writable: false,
-    enumerable: false,
-    configurable: false
-});
+// Object.defineProperty(oscar, 'pruebaNasa', {
+//     value: 'Extraterrestres',
+//     writable: false,
+//     enumerable: false,
+//     configurable: false
+// });
 
-Object.defineProperty(oscar, 'navigator', {
-    value: 'Chrome',
-    writable: true,
-    enumerable: false,
-    configurable: true
-});
+// Object.defineProperty(oscar, 'navigator', {
+//     value: 'Chrome',
+//     writable: true,
+//     enumerable: false,
+//     configurable: true
+// });
 
-Object.defineProperty(oscar, 'editor', {
-    value: 'VSCode',
-    writable: false,
-    enumerable: true,
-    configurable: true
-});
+// Object.defineProperty(oscar, 'editor', {
+//     value: 'VSCode',
+//     writable: false,
+//     enumerable: true,
+//     configurable: true
+// });
 
-Object.defineProperty(oscar, 'terminal', {
-    value: 'WSL',
-    writable: true,
-    enumerable: true,
-    configurable: false
-});
+// Object.defineProperty(oscar, 'terminal', {
+//     value: 'WSL',
+//     writable: true,
+//     enumerable: true,
+//     configurable: false
+// });
+
+// console.log(Object.getOwnPropertyDescriptors(oscar));
+
+//Seal(): Configurable a FALSE y Freeze(): 
+Object.seal(oscar);
+// y Freeze(): Configurable y writable a FALSE
+Object.seal(oscar);
 
 console.log(Object.getOwnPropertyDescriptors(oscar));
