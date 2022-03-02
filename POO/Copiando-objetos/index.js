a5ab421d2314a6421b151673045a7a0d858666f9
oscarf4 <oscarvedo97@gmail.com>
2022-03-02 15:59:20 -0500

    JSON.stringify y parse para copiar objetos, no manejan funciones y métodos

--- POO/Copiando-objetos/index.js
+++ POO/Copiando-objetos/index.js
@@ -15,3 +15,11 @@ for (prop in obj1) {
 
 const obj3 = Object.assign({}, obj1); //También se ve afectado por referencia
 const obj4 = Object.create(obj1); //Hace la copia en _proto_, lo cual trae problemas cuando vayamos a modificar estas propiedades, pues las tomará como nuevas y las tendremos afuera las nuevas y en proto las heredadas. [Trash]
+
+
+//JSON.stringify y JSON.parse: No saben trabajar con métodos ni funciones
+const stringifiedComplexObj = JSON.stringify(obj1);
+console.log(stringifiedComplexObj);
+
+const obj5 = JSON.parse(stringifiedComplexObj);
+console.log(obj5);
